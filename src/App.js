e8437e8e72b5659342096a76471c450759c9c4e6
Guru <guruprasad2511@gmail.com>
2021-06-25 19:04:04 +0530

    final corrections done

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import { Counter } from './features/counter/Counter';
 import './App.css';
 import Header from "./components/Header";
 import Home from "./components/Home";
@@ -18,13 +17,13 @@ function App() {
       <Router>
         <Header />
         <Switch>
-          <Route path="/login">
+          <Route exact path="/">
             <Login />
           </Route>
           <Route path="/detail/:id">
             <Detail />
           </Route>
-          <Route path="/">
+          <Route path="/home">
             <Home />
           </Route>
         </Switch>
